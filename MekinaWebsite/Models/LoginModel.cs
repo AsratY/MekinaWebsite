f5beb822dce8a8a007a7dd56b8b6912ed8aac82e
Asrat Yehombawork <92272260+AsratY@users.noreply.github.com>
2022-12-27 23:09:54 +0300

    Initial commit

---
+++ MekinaWebsite/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MekinaWebsite.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { set; get; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { set; get; }
+    }
+}
